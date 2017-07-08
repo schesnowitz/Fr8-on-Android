e0495c0053d04242a2b305f63aca154a63ebbc91
schesnowitz <steve@chesnowitz.com>
2017-07-08 12:55:46 -0400

    getting u location and email same table

--- app/src/main/java/com/chesnowitz/fr8quote/ConfirmationActivity.java
+++ app/src/main/java/com/chesnowitz/fr8quote/ConfirmationActivity.java
@@ -1,22 +1,83 @@
 package com.chesnowitz.fr8quote;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
 
 public class ConfirmationActivity extends AppCompatActivity {
+
+  private LocationManager locationManager;
+  private LocationListener locationListener;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_confirmation);
+    locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+    locationListener = new LocationListener() {
+      @Override
+      public void onLocationChanged(Location location) {
+        Log.i("Location", location.toString());
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        ParseGeoPoint point = new ParseGeoPoint(location.getLatitude(), location.getLongitude());
+        ParseObject userLocation = new ParseObject("DriverLocation");
+        userLocation.put("score", 5566);
+        userLocation.put("location", point);
+        userLocation.put("email", currentUser.getEmail());
+        userLocation.saveInBackground();
+      }
 
+      @Override
+      public void onStatusChanged(String s, int i, Bundle bundle) {
 
+      }
+
+      @Override
+      public void onProviderEnabled(String s) {
+
+      }
+
+      @Override
+      public void onProviderDisabled(String s) {
+
+      }
+    };
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+      ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+    } else {
+      locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+      if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+              PackageManager.PERMISSION_GRANTED) {
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+      }
+    }
   }
 
   @Override
